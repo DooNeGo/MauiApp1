e1c6c96dfd35236cbd255127442fe3e018f58b51
Матвей Кострома <matveyprostomac@gmail.com>
2024-12-26 18:55:34 +0300

    Добавьте файлы проекта.

---
+++ MauiApp1/MauiApp1/MauiProgramExtensions.cs
@@ -0,0 +1,26 @@
+﻿using CommunityToolkit.Maui;
+using Microsoft.Extensions.Logging;
+
+namespace MauiApp1
+{
+    public static class MauiProgramExtensions
+    {
+        public static MauiAppBuilder UseSharedMauiApp(this MauiAppBuilder builder)
+        {
+            builder
+                .UseMauiApp<App>()
+                .UseMauiCommunityToolkit()
+                .ConfigureFonts(fonts =>
+                {
+                    fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                    fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+                });
+
+#if DEBUG
+    		builder.Logging.AddDebug();
+#endif
+
+            return builder;
+        }
+    }
+}
